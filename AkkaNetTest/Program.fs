ef004dd3e92f2bbd44ca594711dbd0921cd142d5
Vasily Kirichenko <vaskir@gmail.com>
2016-05-08 18:42:34 +0300

    binding redirects
    formatting

--- AkkaNetTest/Program.fs
+++ AkkaNetTest/Program.fs
@@ -12,19 +12,22 @@ let main _ =
     let _actor =
         spawn system "Server" <| fun mb ->
             let startTime = DateTime.UtcNow
-            let rec loop count = actor {
-                let! msg = mb.Receive()
-                //Logging.logInfo mb "Got request."
-                match msg with
-                | Request.Add (x, y) -> 
-                    mb.Sender() <! Response.Added (x + y)
-                    //Logging.logInfo mb "Sent response."
-                if count % 1000 = 0 then
-                    Logging.logInfof mb "%d total, %f /s" 
-                                        count 
-                                        (1000. * float count / (DateTime.UtcNow - startTime).TotalMilliseconds)
-                return! loop (count + 1)
-            }
+            let rec loop count = 
+                actor {
+                    let! msg = mb.Receive()
+                    //Logging.logInfo mb "Got request."
+                    match msg with
+                    | Request.Add (x, y) -> 
+                        mb.Sender() <! Response.Added (x + y)
+                        //Logging.logInfo mb "Sent response."
+                    if count % 1000 = 0 then
+                        Logging.logInfof 
+                            mb 
+                            "%d total, %f /s" 
+                            count 
+                            (1000. * float count / (DateTime.UtcNow - startTime).TotalMilliseconds)
+                    return! loop (count + 1)
+                }
             loop 0
     Console.ReadKey() |> ignore
     0
--- Client/Program.fs
+++ Client/Program.fs
@@ -13,17 +13,19 @@ let main _ =
         spawn system "Client" <| fun (mb: Actor<Response>) ->
             let server = select "akka.tcp://ActorSystem1@wl-legio-17:2551/user/Server" system
             let startTime = DateTime.UtcNow
-            let rec loop count = actor {
-                server <! Request.Add (1, 2)
-                //Logging.logInfo mb "Sent request."
-                let! _msg = mb.Receive()
-                //Logging.logInfo mb "Got response."
-                if count % 1000 = 0 then
-                    Logging.logInfof mb "%d total, %f /s" 
-                                        count 
-                                        (1000. * float count / (DateTime.UtcNow - startTime).TotalMilliseconds)
-                return! loop (count + 1)
-            }
+            let rec loop count = 
+                actor {
+                    server <! Request.Add (1, 2)
+                    //Logging.logInfo mb "Sent request."
+                    let! _msg = mb.Receive()
+                    //Logging.logInfo mb "Got response."
+                    if count % 1000 = 0 then
+                        Logging.logInfof 
+                            mb "%d total, %f /s" 
+                            count 
+                            (1000. * float count / (DateTime.UtcNow - startTime).TotalMilliseconds)
+                    return! loop (count + 1)
+                }
             loop 0
 
     Console.ReadKey() |> ignore
